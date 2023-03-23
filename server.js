09f0f9543cf2825c2ba21578a33de0cfced99385
Alexis Checura <alexischecura@gmail.com>
2023-03-23 15:04:04 -0300

    conect to mongodb

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-const port = process.env.PORT || 3000;
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful!'));
+
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
   console.log(`App running in port ${port}...`);
