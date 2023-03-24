5dfb3a35f8a2d455669cb75525761e1ec095094f
Alexis Checura <alexischecura@gmail.com>
2023-03-24 18:39:53 -0300

    add pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,3 @@
-const { listenerCount } = require('../models/tourModel');
 const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
@@ -21,6 +20,27 @@ exports.getAllTours = async (req, res) => {
     } else {
       query = query.sort('-createdAt');
     }
+
+    // 3) Limiting Fields
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    // 4) Pagination
+    const page = +req.query.page || 1;
+    const limit = +req.query.limit || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
     // Execute Query
 
     const tours = await query;
