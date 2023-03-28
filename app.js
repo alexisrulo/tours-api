2b765ae6970ba905dbab9669233e793e75e94556
Alexis Checura <alexischecura@gmail.com>
2023-03-28 10:30:24 -0300

    add error handler

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
+const AppError = require('./utils/AppError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -25,5 +27,11 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
